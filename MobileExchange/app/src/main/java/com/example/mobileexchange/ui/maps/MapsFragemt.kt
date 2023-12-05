ff1bb9a706dbf105fdadd2e2e7cfe7ab425cd2a8
Kamil Matusz <matuszkamil076@gmail.com>
2023-12-05 10:22:30 +0100

    Added Cantor marker to the Google Maps in Application

--- MobileExchange/app/src/main/java/com/example/mobileexchange/ui/maps/MapsFragemt.kt
+++ MobileExchange/app/src/main/java/com/example/mobileexchange/ui/maps/MapsFragemt.kt
@@ -3,7 +3,6 @@ package com.example.mobileexchange.ui.maps
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
-import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -18,15 +17,22 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest
+import com.google.android.libraries.places.api.net.PlacesClient
+import java.util.*
 
 class MapsFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMapClickListener {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: FragmentMapsBinding
-    private lateinit var lastLocation: Location
+    private lateinit var lastLocation: LatLng
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var placesClient: PlacesClient
 
     companion object {
         private const val LOCATION_REQUEST_CODE = 1
@@ -47,6 +53,10 @@ class MapsFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMapClickListene
         mapFragment.getMapAsync(this)
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+
+        // Inicjalizacja Places API
+        Places.initialize(requireContext(), getString(R.string.google_maps_api_key))
+        placesClient = Places.createClient(requireContext())
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -60,13 +70,14 @@ class MapsFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMapClickListene
     @SuppressLint("MissingPermission")
     private fun setUpMap() {
         if (checkLocationPermissions()) {
-            mMap.isMyLocationEnabled = true
             fusedLocationClient.lastLocation.addOnSuccessListener(requireActivity()) { location ->
                 if (location != null) {
-                    lastLocation = location
-                    val currentLatLong = LatLng(location.latitude, location.longitude)
-                    placeMarkedOnMap(currentLatLong)
-                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLong, 12f))
+                    lastLocation = LatLng(location.latitude, location.longitude)
+                    placeMarkedOnMap(lastLocation)
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(lastLocation, 12f))
+
+                    // Dodawanie znaczników pobliskich kantorów
+                    addNearbyCurrencyExchangeMarkers()
                 }
             }
         }
@@ -98,7 +109,33 @@ class MapsFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMapClickListene
         mMap.addMarker(markerOptions)
     }
 
+    @SuppressLint("MissingPermission")
+    private fun addNearbyCurrencyExchangeMarkers() {
+        val placeFields = listOf(Place.Field.NAME, Place.Field.LAT_LNG)
+        val request = FindCurrentPlaceRequest.builder(placeFields).build()
+
+        placesClient.findCurrentPlace(request).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val likelyPlaces = task.result
+                for (placeLikelihood in likelyPlaces?.placeLikelihoods ?: emptyList()) {
+                    val place = placeLikelihood.place
+                    val placeLatLng = place.latLng
+                    val placeName = place.name
+
+                    // Dodawanie znacznika dla każdego pobliskiego kantoru
+                    placeLatLng?.let {
+                        val markerOptions = MarkerOptions()
+                            .position(it)
+                            .title(placeName)
+                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)) // Ustawienie koloru markera na zielony
+                        mMap.addMarker(markerOptions)
+                    }
+                }
+            }
+        }
+    }
+
     override fun onMapClick(p0: LatLng) {
-        // TODO: Handle map click
+        // TODO: Obsługa kliknięcia na mapie
     }
 }
