5ec62cb00f0179cd3a25b6c9a3dbab0b4481c953
Filip Lepa <pppfkp@outlook.com>
2023-12-14 11:28:13 +0100

    Added display logic in BillRecognitionFragment and loaded an example tflite model that recognizes landmarks

--- MobileExchange/app/src/main/java/com/example/mobileexchange/ui/bill_recognition/BillRecognitionFragment.kt
+++ MobileExchange/app/src/main/java/com/example/mobileexchange/ui/bill_recognition/BillRecognitionFragment.kt
@@ -6,7 +6,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Recomposer
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import com.example.mobileexchange.R
+import com.example.mobileexchange.image_classification.data.TfLiteBillClassifier
+import com.example.mobileexchange.image_classification.domain.Classification
+import com.example.mobileexchange.image_classification.presentation.BillImageAnalyzer
+import com.example.mobileexchange.image_classification.presentation.CameraPreview
+import com.example.mobileexchange.image_classification.ui.BillRecognitionTensorflowTheme
 
 class BillRecognitionFragment : Fragment() {
 
@@ -19,8 +47,61 @@ class BillRecognitionFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_bill_recognition, container, false)
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                BillRecognitionTensorflowTheme {
+                    val applicationContext = context
+                    var classifications by remember {
+                        mutableStateOf(emptyList<Classification>())
+                    }
+                    val analyzer = remember {
+                        BillImageAnalyzer(
+                            classifier = TfLiteBillClassifier(
+                                context = applicationContext
+                            ),
+                            onResults = {
+                                classifications = it
+                            }
+                        )
+                    }
+                    val controller = remember {
+                        LifecycleCameraController(applicationContext).apply {
+                            setEnabledUseCases(CameraController.IMAGE_ANALYSIS)
+                            setImageAnalysisAnalyzer(
+                                ContextCompat.getMainExecutor(applicationContext),
+                                analyzer
+                            )
+                        }
+                    }
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                    ) {
+                        CameraPreview(controller, Modifier.fillMaxSize())
+
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .align(Alignment.TopCenter)
+                        ) {
+                            classifications.forEach {
+                                Text(
+                                    text = it.name,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .background(MaterialTheme.colorScheme.primaryContainer)
+                                        .padding(8.dp),
+                                    textAlign = TextAlign.Center,
+                                    fontSize = 20.sp,
+                                    color = MaterialTheme.colorScheme.primary
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
